27d05b3e91a98bcd9a3315093df1628940b92764
Benoit Delboé <benoit@lescanumeriques.fr>
2022-05-19 15:28:19 +0200

    first commit

---
+++ coral-guardian-certificate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Adopte un corail / certificate
+ * Plugin URI:
+ * Description: Gestion des certificats
+ * Version: 0.1
+ * Requires PHP: 8.1
+ * Author: Benoit DELBOE & Grégory COLLIN
+ * Author URI:
+ * Licence: GPLv2
+ */
+add_action('plugins_loaded', [\D4rk0snet\Certificate\Plugin::class, 'launchActions']);
---
+++ src/Plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace D4rk0snet\Certificate;
+
+class Plugin
+{
+    public static function launchActions()
+    {
+    }
+}
